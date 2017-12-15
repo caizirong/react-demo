c2d30db77616049bc8071fa029fa9e020709b150
caizirong <caizirongczr@gmail.com>
2017-12-15 10:39:39 +0800

    update

--- chunked_with_redux/src/Routes.js
+++ chunked_with_redux/src/Routes.js
@@ -1,4 +1,9 @@
+import React from 'react'
+import {Router, Route, IndexRoute, browserHistory} from 'react-router'
+import {Provider} from 'redux'
+import {combineReducers} from 'redux'
 
+import {syncHistoryWithStore} from 'react-router-redux'
 
 
 
